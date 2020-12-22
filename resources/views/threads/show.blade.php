bf5f98d9cdf3bfb0be0922f36d2bd0eb5083b111
Mitch <mjames962@hotmail.com>
2020-12-22 13:58:24 +0000

    See comments under a thread.
    Signed-off-by: Mitch <mjames962@hotmail.com>

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,15 +1,19 @@
 @extends('layouts.app')
 
-@section('title', 'Thread Number: '. $thread->id)
+@section('title', 'Thread Number: '. $thread->id . ' ('. $thread->created_at . ')')
 
 @section('content')
-
+    
     <h2>
-        {{ $thread->title }}
+       {{ $thread->title }}
     </h2>
 
-    <p>
-        {{ $thread->body }}
-    </p>
+    <p>{{ $thread->body }}</p>
+
+    @foreach ($thread->comments as $comment)    
+        <br>
+        <p>{{ $comment->created_at }}</p>
+        <p>{{ $comment->body }}</p>
+    @endforeach
 
 @endsection
